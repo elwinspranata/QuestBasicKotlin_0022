78ceaeb2013b2705c68439ccd2fe33f2fabe5a61
elwinspranata <elwinpranata02@gmail.com>
2024-10-12 01:18:03 +0700

    contact: Ini adalah objek dari sebuah kelas (misalnya, Contact) yang memiliki properti email. Objek ini harus sudah dideklarasikan dan diinisialisasi sebelumnya.

--- app/src/main/java/com/example/praktikum1/Class.kt
+++ app/src/main/java/com/example/praktikum1/Class.kt
@@ -1,3 +1,11 @@
 package com.example.praktikum1
 
 class Motor()
+
+class Contact (val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+// Prints the value of the property: email
+    println(contact.email)
