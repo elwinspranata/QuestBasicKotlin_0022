6bbbea69e428fb8a8a0c76b8c80a00504bc507af
elwinspranata <elwinpranata02@gmail.com>
2024-10-12 10:46:54 +0700

    Null Safety: Kotlin mencegah penugasan nilai null ke variabel bertipe non-null (seperti String). Ini membantu mencegah NullPointerException yang sering menjadi masalah di banyak bahasa pemrograman lain.

---
+++ app/src/main/java/com/example/praktikum1/NullSafety.kt
@@ -0,0 +1,18 @@
+package com.example.praktikum1
+
+fun nullsafety() {
+// neverNull has String type
+    var neverNull: String = "This can't be null"
+// Throws a compiler error
+// neverNull = null
+// nullable has nullable String type
+    var nullable: String? = "You can keep a null her e"
+// This is OK
+    nullable = null
+// Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+    
